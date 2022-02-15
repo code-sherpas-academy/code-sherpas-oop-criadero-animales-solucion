eb55458b60f0a842fda40530b2611c6ae562ed4d
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2022-02-15 13:09:17 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.3.3/userguide/multi_project_builds.html
  */
 
-rootProject.name = "sample"
+rootProject.name = "code-sherpas-oop-criadero-animales-solucion"
 include("lib")
