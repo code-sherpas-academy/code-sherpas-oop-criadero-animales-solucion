c189fa67f27dd9f63ffde87fea41d882757fc56d
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2022-02-15 14:13:57 -0600

    change

---
+++ lib/src/main/kotlin/sample/CriaderoAnimales.kt
@@ -0,0 +1,40 @@
+package sample
+
+import java.util.Hashtable
+import java.util.UUID
+
+private val dogRepository: Hashtable<UUID, Dog> = Hashtable()
+
+fun main() {
+    var nextLine: String?
+    nextLine = chooseOption()
+
+    while (nextLine != null) {
+
+        when(recognizeOption(nextLine)) {
+            1 -> createDog()
+        }
+
+        nextLine = chooseOption()
+    }
+}
+
+fun createDog() {
+    //val scanner = Scanner(System.`in`)
+    dogRepository[UUID.randomUUID()] = Dog()
+}
+
+private fun recognizeOption(nextLine: String): Int {
+    val words: List<String> = nextLine.split(" ")
+    return words[0].toInt()
+}
+
+private fun chooseOption(): String? {
+    printOptions()
+    return readLine()
+}
+
+private fun printOptions() {
+    println("Elija una opción:")
+    println("\t1. Dar de alta un perro")
+}
