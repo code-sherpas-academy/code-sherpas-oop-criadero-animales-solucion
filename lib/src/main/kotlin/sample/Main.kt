6b0075b1b548f4dd3a2ac3aef0ea0bc7c47c38f2
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2022-02-16 19:30:40 -0600

    ver todos los perros

--- lib/src/main/kotlin/sample/Main.kt
+++ lib/src/main/kotlin/sample/Main.kt
@@ -7,7 +7,16 @@ fun main() {
     while (option != null) {
 
         when(option) {
-            1 -> createDog()
+            1 -> {
+                println("Introduce los campos del perro. Por ejemplo:")
+                println("\tname=Teddy breed=Yorkshire birthdate=2020-03-05 father=23 mother=")
+                createDog()
+            }
+            2 -> {
+                getDogs().forEach { dog: Dog ->
+                    println("Dog: id=${dog.id} name=${dog.name} father=${dog.father?.id ?: ""} mother=${dog.mother?.id ?: ""}")
+                }
+            }
         }
 
         option = readOption()
@@ -23,4 +32,5 @@ private fun readOption(): Int? {
 private fun printOptions() {
     println("Elija una opción:")
     println("\t1 - Crear un perro")
+    println("\t2 - Ver todos los perros")
 }
